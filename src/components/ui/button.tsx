cceb62e9810b24d2512cf1c16cfd057691dfbf13
Maxim <schubert.walk@yandex.ru>
2024-08-27 17:50:20 +0300

    feat: Add children to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -33,14 +33,18 @@ const buttonVariants = cva(
 export interface ButtonProps
   extends React.ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
+  className?: string;
   asChild?: boolean;
+  children?: React.ReactNode | string;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, ...props }, ref) => {
+  ({ className, children, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button";
     return (
-      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props} />
+      <Comp className={cn(buttonVariants({ variant, size, className }))} ref={ref} {...props}>
+        {children}
+      </Comp>
     );
   }
 );
